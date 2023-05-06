4834e2b7571e5cfc74619b7cba3c6995420524ff
ezhoden <ezhoden@main.ru>
2023-05-06 20:44:39 +0400

    update layout

--- keyboards/ferris/keymaps/ezhoden/keymap.c
+++ keyboards/ferris/keymaps/ezhoden/keymap.c
@@ -49,22 +49,22 @@ enum ferris_tap_dances {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [QWERTY] = LAYOUT( /* QWERTY */
     KC_QESC, KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,  KC_I,    KC_O,   KC_P,
-    KC_ATAB, KC_SSFT, KC_D,    KC_FALT, KC_GGUI,         KC_HGUI,    KC_JALT,  KC_KCTL,    KC_LRSH,   KC_SCLE,
+    KC_ATAB, KC_SSFT, KC_D,    KC_FALT, KC_GGUI,         KC_HGUI,    KC_JALT,  KC_KCTL,    KC_LRSH,   KC_SCLN,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,  KC_COMM, KC_DOT, KC_SLSH,
-                                    KC_CLGV, KC_SPM2, KC_BSM1, KC_BSLS
+                                    KC_CLGV, KC_SPM2, KC_BSM1, KC_ENT
   ),
 
   [_LOWER] = LAYOUT( /* LOWER */
     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-    KC_GESC, KC_HOME, KC_PGDN, KC_PGUP, KC_END,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,         KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_QUOT,
+    KC_GESC, KC_HOME, KC_PGDN, KC_PGUP, KC_END,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,         KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BSLS,
                                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
   [_RAISE] = LAYOUT( /* RAISE */
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-    KC_TAB,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_PIPE,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_UNDS, KC_PLUS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TAB,  KC_TRNS, KC_TRNS, KC_TRNS, KC_F11,         KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_PIPE,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F12,         KC_UNDS, KC_PLUS, KC_TRNS, KC_TRNS, KC_TRNS,
                                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   )
 };
@@ -74,5 +74,4 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     // Tap once for Q, twice for ESC
     [TD_Q_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_ESC),
     [TD_A_TAB] = ACTION_TAP_DANCE_DOUBLE(KC_A, KC_TAB),
-    [TD_SEMICOLON_ENTER] = ACTION_TAP_DANCE_DOUBLE(KC_SCLN, KC_ENT)
 };
