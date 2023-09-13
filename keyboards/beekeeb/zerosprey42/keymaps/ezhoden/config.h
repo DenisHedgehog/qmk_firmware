86a653ab1bd3d35bfae78fae21b23575e607f9e2
ezhoden <ezhoden@main.ru>
2023-09-14 03:05:11 +0400

    layout improvements

--- keyboards/beekeeb/zerosprey42/keymaps/ezhoden/config.h
+++ keyboards/beekeeb/zerosprey42/keymaps/ezhoden/config.h
@@ -16,7 +16,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
-
-// Pick good defaults for enabling homerow modifiers
-#define TAPPING_TERM 200
-#define IGNORE_MOD_TAP_INTERRUPT
--- keyboards/beekeeb/zerosprey42/keymaps/ezhoden/keymap.c
+++ keyboards/beekeeb/zerosprey42/keymaps/ezhoden/keymap.c
@@ -11,16 +11,6 @@ enum ferris_layers {
   _RAISE
 };
 
-enum ferris_tap_dances {
-  TD_Q_ESC,
-  TD_A_TAB
-};
-
-// Tap dance key code definitions
-#define KC_QESC TD(TD_Q_ESC)
-#define KC_ATAB TD(TD_A_TAB)
-#define KC_SCLE TD(TD_SEMICOLON_ENTER)
-
 // Layers key code definitions
 #define KC_SPM2 LT(2, KC_SPC)
 #define KC_BSM1 LT(1, KC_BSPC)
@@ -47,30 +37,23 @@ enum ferris_tap_dances {
 // Keymap
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [QWERTY] = LAYOUT_split_3x6_3( /* QWERTY */
-    KC_ESC, KC_Q, KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,  KC_I,    KC_O,   KC_P, KC_LBRC,
-    KC_LCTL, KC_ATAB, KC_SSFT, KC_D,    KC_FALT, KC_GGUI,         KC_HGUI,    KC_JALT,  KC_KCTL,    KC_LRSH,   KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RBRC,
-                                 KC_TAB, KC_CLGV, KC_SPM2, KC_BSM1, KC_ENT, KC_LGUI
+    KC_PSCR, KC_Q, KC_W,    KC_E,    KC_R,    KC_T,            KC_Y,    KC_U,  KC_I,    KC_O,   KC_P, KC_LBRC,
+    KC_ESC, KC_A, KC_S, KC_D,    KC_F, KC_G,                  KC_H,    KC_J,  KC_K,    KC_L,   KC_SCLN, KC_QUOT,
+    KC_LALT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,            KC_N,    KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_RBRC,
+                                 OSM(MOD_LSFT), KC_CLGV, KC_SPM2, KC_BSM1, KC_ENT, KC_LGUI
   ),
 
   [_LOWER] = LAYOUT_split_3x6_3( /* LOWER */
-    KC_QESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_F10,
-    KC_QESC, KC_ESC, KC_HOME, KC_PGDN, KC_PGUP, KC_END,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT, KC_F10,
-    KC_QESC, KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,         KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BSLS, KC_F10,
+    KC_CAPS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,            KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_F10,
+    KC_CAPS, KC_ESC, KC_HOME, KC_PGDN, KC_PGUP, KC_END,          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_QUOT, KC_F10,
+    KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,         KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BSLS, KC_F10,
                                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
   [_RAISE] = LAYOUT_split_3x6_3( /* RAISE */
-    KC_QESC, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_F10,
-    KC_QESC, KC_TAB,  KC_TRNS, KC_TRNS, KC_TRNS, KC_F11,         KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_PIPE, KC_F10,
-    KC_QESC, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F12,         KC_UNDS, KC_PLUS, KC_TRNS, KC_TRNS, KC_GRV, QK_BOOT,
+    KC_CAPS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_F10,
+    KC_CAPS, KC_TAB,  KC_TRNS, KC_TRNS, KC_TRNS, KC_F11,         KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_PIPE, KC_F10,
+    KC_CAPS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_F12,         KC_UNDS, KC_PLUS, KC_TRNS, KC_TRNS, KC_GRV, QK_BOOT,
                                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   )
-};
-
-// Tap dance actions definition
-tap_dance_action_t tap_dance_actions[] = {
-    // Tap once for Q, twice for ESC
-    [TD_Q_ESC] = ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_ESC),
-    [TD_A_TAB] = ACTION_TAP_DANCE_DOUBLE(KC_A, KC_TAB),
 };
